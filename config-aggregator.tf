d7b1a3da36612bab08bb0eb41ae374dcf15a4eee
terralien <terralienjp@gmail.com>
2023-03-18 19:55:05 +0900

    add config-aggregator

---
+++ config-aggregator.tf
@@ -0,0 +1,13 @@
+resource "aws_config_configuration_aggregator" "this" {
+  name = "aggregator-this"
+  
+  account_aggregation_source {
+    account_ids = ["123456789012"]
+    all_regions = true
+  }
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
